c0629f40dd0991dcc0385464f59da6b2a64155fd
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-06-13 17:09:20 +0700

    rename file extension

--- tugaswebahmadpahru/navbar.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<div class="topnav">
-    <a class="active" href="home.html">Home</a>
-    <a href="login.html">Login</a>
-    <a href="register.html">Register</a>
-    <a href="quiz.html">Quiz</a>
-</div>
