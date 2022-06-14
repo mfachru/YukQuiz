251ed1f9db615089e9afb536bcf356466b28af8e
Muhammad Fachru Syahputra <fachrusyahputra@gmail.com>
2022-06-14 20:19:51 +0700

    ubah login

--- tugaswebahmadpahru/index.php
+++ tugaswebahmadpahru/index.php
@@ -1,21 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>About Me</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="style.css" rel="stylesheet">
-</head>
-
-<body class="mainbackground">
-  <div class="topnav">
-      <a class="active" href="home.html">Home</a>
-      <a href="login.html">Login</a>
-      <a href="register.html">Register</a>
-      <a href="quiz.html">Quiz</a>
-  </div>
-</body>
-
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Halaman Login</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+   
+    <body class="loginbg">
+        <div class="container">
+          <h1>Login</h1>
+            <form>
+                <label>Username</label><br>
+                <input type="text"><br>
+                <label>Password</label><br>
+                <input type="password"><br>
+                <button>Log in</button>
+            </form>
+        </div>     
+    </body>
 </html>
