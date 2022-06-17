ba42984b92211d18108c7dbb9f902a5854e7be53
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-06-17 10:41:00 +0700

    login

--- tugaswebahmadpahru/index.php
+++ tugaswebahmadpahru/index.php
@@ -3,7 +3,22 @@ require 'config.php';
 require 'preConfig.php';
 session_start();
 if(isset($_POST['login'])){
-    header("Location: home.html");
+    $email = $_POST['email'];
+    $passwords = $_POST['passwords'];
+
+    $sql = "SELECT * FROM user WHERE email='$email' AND passwords='$passwords'";
+    $result = mysqli_query($host, $sql);
+    if(mysqli_num_rows($result) > 0){
+        while($row=mysqli_fetch_assoc($result)){
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['passwords'] = $row['passwords'];
+            $_SESSION['about_me'] = $row['about_me'];
+        }
+        header("Location: home.html");
+    } else {
+        echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
+    }
 }
 ?>
 
@@ -20,9 +35,9 @@ if(isset($_POST['login'])){
           <h1>Login</h1>
             <form action="index.php" method="POST">
                 <label>Email</label><br>
-                <input type="email" required><br>
+                <input type="email" name="email" required><br>
                 <label>Password</label><br>
-                <input type="password" required><br>
+                <input type="password" name="passwords" required><br>
                 <button name="login">Log in</button>
                 <p>Anda belum punya akun? <a href="register.php">Register</a></p>
             </form>
--- tugaswebahmadpahru/register.php
+++ tugaswebahmadpahru/register.php
@@ -11,9 +11,9 @@
           <h1>Register</h1>
             <form action="register.php" method="POST">
                 <label>Email</label><br>
-                <input type="email" required><br>
+                <input type="email" name="email" required><br>
                 <label>Password</label><br>
-                <input type="password" required><br>
+                <input type="password" name="passwords" required><br>
                 <button name="register">Register</button>
                 <p>Anda sudah punya akun? <a href="index.php">Login</a></p>
             </form>
@@ -23,7 +23,16 @@
 
 <?php
 session_start();
+require 'config.php';
 if(isset($_POST['login'])){
-    header("Location: home.html");
+    $email = $_POST['email'];
+    $passwords = $_POST['passwords'];
+    $cek = mysqli_query($host, "SELECT * FROM user WHERE email='$email' AND passwords='$passwords'");
+    if(mysqli_num_rows($cek)>0){
+        echo "<script>alert('Email sudah terdaftar\')</script>";
+    } else {
+        $ins = mysqli_query($host, "");
+        header("location: index.php");
+    }
 }
 ?>
