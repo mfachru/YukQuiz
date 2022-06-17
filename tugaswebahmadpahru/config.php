8888a77ff7dda444fe5e0308450a013ce4b542da
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-06-17 09:06:09 +0700

    try catch

--- tugaswebahmadpahru/config.php
+++ tugaswebahmadpahru/config.php
@@ -1,9 +1,14 @@
 <?php
-if (!$host = mysqli_connect("localhost", "root", "", "uap_web")) {
-    $hosting = mysqli_connect("localhost", "root", "");
-    mysqli_query($hosting, "CREATE DATABASE uap_web");
-    if(!$hosting){
-        die("Koneksi Gagal: " . mysqli_connect_error());
+try{
+    $host = mysqli_connect("localhost", "root", "", "uap_web");
+}
+catch(Exception $e){
+    try{
+        $hosting = mysqli_connect("localhost", "root", "");
+        mysqli_query($hosting, "CREATE DATABASE uap_web");
+    }
+    catch(Exception $e){
+        echo "Connection failed: " . mysqli_connect_error();
     }
 }
 ?>
