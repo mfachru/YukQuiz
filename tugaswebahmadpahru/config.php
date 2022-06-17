939105cf2acb838916776812c4a46bb6a7976410
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-06-17 09:09:26 +0700

    simple trycatch

--- tugaswebahmadpahru/config.php
+++ tugaswebahmadpahru/config.php
@@ -1,14 +1,10 @@
 <?php
 try{
+    $hosting = mysqli_connect("localhost", "root", "");
+    mysqli_query($hosting, "CREATE DATABASE IF NOT EXISTS uap_web");
     $host = mysqli_connect("localhost", "root", "", "uap_web");
 }
 catch(Exception $e){
-    try{
-        $hosting = mysqli_connect("localhost", "root", "");
-        mysqli_query($hosting, "CREATE DATABASE uap_web");
-    }
-    catch(Exception $e){
-        echo "Connection failed: " . mysqli_connect_error();
-    }
+    echo "Connection failed: " . mysqli_connect_error();
 }
 ?>
