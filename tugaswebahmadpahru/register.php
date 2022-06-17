3ceef4c88492dfda4104a84ee7469bc1d640596a
Ahmad Al Farizi <a.farizi48@gmail.com>
2022-06-17 08:15:43 +0700

    big update

--- tugaswebahmadpahru/register.php
+++ tugaswebahmadpahru/register.php
@@ -1,21 +1,21 @@
 <!DOCTYPE HTML>
 <html>
     <head>
-        <title>Halaman Login</title>
+        <title>Halaman Register</title>
         <link rel="shortcut icon" href="unila.ico"/>
         <link rel="stylesheet" href="style.css">
     </head>
    
     <body class="loginbg">
         <div class="container">
-          <h1>Login</h1>
-            <form action="index.php" method="POST">
+          <h1>Register</h1>
+            <form action="register.php" method="POST">
                 <label>Email</label><br>
                 <input type="email" required><br>
                 <label>Password</label><br>
                 <input type="password" required><br>
-                <button name="login">Log in</button>
-                <p>Anda belum punya akun? <a href="register.php">Register</a></p>
+                <button name="register">Register</button>
+                <p>Anda sudah punya akun? <a href="index.php">Login</a></p>
             </form>
         </div>     
     </body>
