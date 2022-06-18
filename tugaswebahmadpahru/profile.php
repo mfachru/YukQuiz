4b513ba94dce1a4d9a591e9b1c6f555414f2b704
Muhammad Fachru Syahputra <fachrusyahputra@gmail.com>
2022-06-18 14:40:45 +0700

    update 18 juni

---
+++ tugaswebahmadpahru/profile.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="unila.ico"/>
+        <title>Home</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="topnav">
+            <a href="home.html">Home</a>
+            <a href="quiz.html">Quiz</a>
+            <a class="active" href="profile.php">Profile</a>
+            <a href="index.php">Log Out</a>
+        </div>
+    </body>
+    
+    <body>
+        <h2 style="text-align:center">Your Profile</h2>
+        <div class="card">
+            <img class="profileimg" src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/05/gaten-matarazzo-stranger-things.jpg" alt="John" style="width:100%">
+            <h1 class="profilename" >Dustin Henderson</h1>
+            <p class="title"><i>Never tell me the odds</i></p>
+            <p class="pr1" ><button class="buttonprofile" >Update Account</button></p>
+            <p><button class="buttonprofile" >Delete Account</button></p>
+        </div>
+    </body>
+</html>
