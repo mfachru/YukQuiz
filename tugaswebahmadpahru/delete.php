6a4d3b03d983b027c47db44bbb45f10d5e25b6d3
Ahmad Al Farizi <74691228+Ahmad-Kun@users.noreply.github.com>
2022-06-19 07:51:16 +0700

    Add files via upload

---
+++ tugaswebahmadpahru/delete.php
@@ -0,0 +1,11 @@
+<?php
+require 'config.php';
+session_start();
+$email = $_SESSION['email'];
+$sql = "DELETE FROM user WHERE email='$email'";
+$result = mysqli_query($host, $sql);
+if($result){
+    session_destroy();
+    echo "<script>alert('$email')</script>";
+}
+?>
